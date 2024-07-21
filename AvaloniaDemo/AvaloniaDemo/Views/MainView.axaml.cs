f57dc152892774044d4869dba70e7e437daab225
LiuChao19940201 <13487011994.com>
2024-07-21 19:10:49 +0800

    2

--- AvaloniaDemo/AvaloniaDemo/Views/MainView.axaml.cs
+++ AvaloniaDemo/AvaloniaDemo/Views/MainView.axaml.cs
@@ -1,12 +1,35 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Interactivity;
+using System;
 
 namespace AvaloniaDemo.Views
 {
     public partial class MainView : UserControl
     {
+        private WindowNotificationManager? _manager;
+
         public MainView()
         {
             InitializeComponent();
         }
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+            var topLevel = TopLevel.GetTopLevel(this);
+            _manager = new WindowNotificationManager(topLevel) { MaxItems = 3 };
+        }
+
+        private void InfoButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            if (sender is Button b && b.Content is string s)
+            {
+                _manager?.Show(Enum.TryParse<NotificationType>(s, out NotificationType t)
+                    ? new Notification(t.ToString(), "This is message", t)
+                    : new Notification(s, "This is message"));
+            }
+        }
+
     }
 }
