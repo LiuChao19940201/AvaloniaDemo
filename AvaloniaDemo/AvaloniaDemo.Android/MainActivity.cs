6498b12c8960c3fec74c47c1aabc233e8f68e3c2
LiuChao19940201 <13487011994@163.com>
2026-03-15 00:42:06 +0800

    升级到.net 10版本

--- AvaloniaDemo/AvaloniaDemo.Android/MainActivity.cs
+++ AvaloniaDemo/AvaloniaDemo.Android/MainActivity.cs
@@ -1,5 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using AndroidX.Core.View;
 using Avalonia;
 using Avalonia.Android;
 using Avalonia.ReactiveUI;
@@ -21,15 +24,32 @@ namespace AvaloniaDemo.Android
                 //.WithFont_Roboto()
                 .UseReactiveUI();
         }
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // 设置状态栏透明
+            if (Window != null)
+            {
+                Window.SetStatusBarColor(Color.Transparent);
+
+                // 让内容延伸到状态栏下面
+                WindowCompat.SetDecorFitsSystemWindows(Window, false);
+            }
+        }
     }
 
     /*
         
-         # 清理项目
-         dotnet clean TestAva.Android.csproj -c Release
+        #先切到 Android 项目目录
+        cd AvaloniaDemo/AvaloniaDemo.Android
 
-         # 构建并打包 Android 项目（以 net8.0-android 为例）
-         dotnet build AvaloniaDemo.Android.csproj -c Release -f:net8.0-android -r android-arm64 -p:AndroidPackageFormat=apk --self-contained false
+         # 清理项目
+         dotnet clean AvaloniaDemo.Android.csproj -c Release
+
+         # 构建并打包 Android 项目（以 net10.0-android 为例）
+         dotnet build AvaloniaDemo.Android.csproj -c Release -f:net10.0-android -r android-arm64 -p:AndroidPackageFormat=apk --self-contained false
     
      */
 }
